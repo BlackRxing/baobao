7bdf8b76f652d9c16f6b5040118d62409a06cd61
ricky <galxy1998922@163.com>
2019-10-31 16:46:59 +0800

    首页底部导航栏，UIxml布局FrameLayout确定

--- app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/MainActivity.java
+++ app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/MainActivity.java
@@ -1,5 +1,22 @@
 package com.example.baoxiaojianapp.baoxiaojianapp.activity;
 
-public class MainActivity {
+import android.os.Bundle;
 
+import com.example.baoxiaojianapp.R;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends BaseActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_main;
+    }
 }
