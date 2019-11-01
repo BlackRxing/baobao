963568517ba91a595df56ec5aefc6431b68f8cc9
ricky <galxy1998922@163.com>
2019-11-01 13:25:25 +0800

    点击底部导航栏完成fragment切换

--- app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/MainActivity.java
+++ app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/MainActivity.java
@@ -17,53 +17,84 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private FrameLayout mainframelayout;
     private BottomNavigationView bottomNavigationView;
     private AppraisalFragment appraisalFragmentProvider;
     private PersonFragment personFragmentProvider;
     private List<Fragment> fragments;
-    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switchfragment(menuItem.getItemId());
-            return true;
-        }
-    };
+    private int lastFragment=0;
 
-    private void initFragments(){
-        fragments=new ArrayList<>();
-        fragments.add(new AppraisalFragment());
-        fragments.add(new PersonFragment());
-    }
-
-    private void switchfragment(int itemId){
-        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-        switch (itemId){
-            case R.id.main_appraisal:
-                fragmentTransaction.replace(R.id.fragment_frame,new AppraisalFragment());
-                break;
-            case R.id.main_person:
-                fragmentTransaction.replace(R.id.fragment_frame,new PersonFragment());
-                break;
-        }
-    }
-    private void setFragmentsPosition(int position){
-        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.show(fragments.get(position));
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindView();
+        init();
     }
+
+    private void init(){
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        initFragments();
+    }
+
+    private void bindView(){
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+    }
+
+    private void initFragments(){
+        fragments=new ArrayList<>();
+        fragments.add(new AppraisalFragment());
+        fragments.add(new PersonFragment());
+        lastFragment=0;
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_frame,fragments.get(0)).show(fragments.get(0)).commit();
+    }
+
+    private void switchfragment(){
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        if(lastFragment==0){
+            lastFragment=1;
+            fragmentTransaction.hide(fragments.get(0));
+            if (!fragments.get(1).isAdded()) {
+                fragmentTransaction.add(R.id.fragment_frame,fragments.get(1));
+            }
+            fragmentTransaction.show(fragments.get(1)).commit();
+        }else {
+            lastFragment=0;
+            fragmentTransaction.hide(fragments.get(1));
+            if (!fragments.get(0).isAdded()) {
+                fragmentTransaction.add(R.id.fragment_frame,fragments.get(0));
+            }
+            fragmentTransaction.show(fragments.get(0)).commit();
+        }
+    }
+
+
+
 
 
 
     @Override
     public int getLayoutResId() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.main_appraisal:
+                if(lastFragment!=0){
+                    switchfragment();
+                }
+                return true;
+            case R.id.main_person:
+                if(lastFragment!=1){
+                    switchfragment();
+                }
+                return true;
+        }
+        return false;
     }
 }
