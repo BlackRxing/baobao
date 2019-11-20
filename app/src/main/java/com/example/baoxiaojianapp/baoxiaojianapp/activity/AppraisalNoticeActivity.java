6555270fea1a9dcaa1a088b99f16c0a86b03511a
ricky <galxy1998922@163.com>
2019-11-20 13:15:38 +0800

    鉴定须知页完成

--- app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/AppraisalNoticeActivity.java
+++ app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/AppraisalNoticeActivity.java
@@ -1,6 +1,7 @@
 package com.example.baoxiaojianapp.baoxiaojianapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -21,11 +22,20 @@ import com.bumptech.glide.Glide;
 import com.example.baoxiaojianapp.R;
 import com.example.baoxiaojianapp.baoxiaojianapp.Utils.NetInterface;
 import com.example.baoxiaojianapp.baoxiaojianapp.Utils.OkHttpUtils;
+import com.example.baoxiaojianapp.baoxiaojianapp.adapter.AppraisalPointAdapter;
+import com.example.baoxiaojianapp.baoxiaojianapp.classpakage.AppraisalPointItem;
 import com.example.baoxiaojianapp.baoxiaojianapp.classpakage.LoginRequest;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppraisalNoticeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,8 +51,8 @@ public class AppraisalNoticeActivity extends AppCompatActivity implements View.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appraisal_notice);
         bindView();
-        Bundle bundle=getIntent().getExtras();
-        initView(bundle.getString("brandName"),bundle.getString("imageUrl"),bundle.getString("kindKey"));
+        Bundle bundle = getIntent().getExtras();
+        initView(bundle.getString("brandName"), bundle.getString("imageUrl"), bundle.getString("kindKey"));
     }
 
     private void bindView() {
@@ -50,14 +60,17 @@ public class AppraisalNoticeActivity extends AppCompatActivity implements View.O
         brandImage = findViewById(R.id.brandImage);
         brandText = findViewById(R.id.brandName);
         classText = findViewById(R.id.className);
-        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.appraisal_point_recyclerView);
         appraisalButton = findViewById(R.id.appraisal_button);
+        backLayout=findViewById(R.id.back_layout);
+        backLayout.setOnClickListener(this);
+        appraisalButton.setOnClickListener(this);
     }
 
-    public void initView(String brandName,String imageUrl,String kindKey){
+    public void initView(String brandName, String imageUrl, String kindKey) {
         Glide.with(this).load(imageUrl).centerCrop().into(brandImage);
         brandText.setText(brandName);
-        switch (kindKey){
+        switch (kindKey) {
             case "shoe":
                 classText.setText("#鞋履");
                 break;
@@ -69,18 +82,24 @@ public class AppraisalNoticeActivity extends AppCompatActivity implements View.O
                 break;
         }
 
-        JsonObject jsonObject=new JsonObject();
-        jsonObject.addProperty("brand",brandName);
-        final RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"),jsonObject.toString());
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("brand", brandName);
+        final RequestBody requestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonObject.toString());
         OkHttpUtils okHttpUtils = OkHttpUtils.getInstance();
         okHttpUtils.post(NetInterface.TSAppraisalProcessRequest, requestBody, new OkHttpUtils.RealCallback() {
             @Override
             public void onResponse(Call call, Response response) {
                 try {
-                    bindData(response.body().string());
-                }catch(IOException e){
+                    JSONObject jsonObject = new JSONObject(response.body().string());
+                    JSONArray jsonArray = jsonObject.getJSONArray("pointList");
+                    bindData(jsonArray);
+                } catch (IOException e) {
+
+                } catch (JSONException j) {
+
                 }
             }
+
             @Override
             public void onFailure(Call call, IOException e) {
 
@@ -88,16 +107,40 @@ public class AppraisalNoticeActivity extends AppCompatActivity implements View.O
         }, true);
     }
 
-    private void bindData(String json){
+    private void bindData(JSONArray jsonArray) {
+        List<AppraisalPointItem> appraisalPointItemList = new ArrayList<>();
+        int points = jsonArray.length();
+        Log.i("this",jsonArray.toString());
+        String image_Url="";
+        String title="";
+        for (int i=0;i < points; i++) {
+            try {
+                image_Url=jsonArray.getJSONObject(i).getString("image_url");
+                title=jsonArray.getJSONObject(i).getString("title");
+            } catch (JSONException j) {
+                j.printStackTrace();
+            }
+            AppraisalPointItem appraisalPointItem=new AppraisalPointItem();
+            appraisalPointItem.setPointtext(title);
+            appraisalPointItem.setPointimageUrl(image_Url);
+            appraisalPointItemList.add(appraisalPointItem);
+        }
 
+        AppraisalPointAdapter appraisalPointAdapter = new AppraisalPointAdapter(appraisalPointItemList);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(appraisalPointAdapter);
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.back_layout:
                 finish();
+                break;
+            case R.id.appraisal_button:
+                ToastUtils.showShort("goto appraisal");
                 break;
         }
     }
---
+++ app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/classpakage/AppraisalPointItem.java
@@ -0,0 +1,22 @@
+package com.example.baoxiaojianapp.baoxiaojianapp.classpakage;
+
+public class AppraisalPointItem {
+    public String getPointimageUrl() {
+        return pointimageUrl;
+    }
+
+    public void setPointimageUrl(String pointimageUrl) {
+        this.pointimageUrl = pointimageUrl;
+    }
+
+    public String getPointtext() {
+        return pointtext;
+    }
+
+    public void setPointtext(String pointtext) {
+        this.pointtext = pointtext;
+    }
+
+    private String pointimageUrl;
+    private String pointtext;
+}
