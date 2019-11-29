03bc4a1351fdb13d2b79457686ab09396095e617
ricky <galxy1998922@163.com>
2019-11-29 17:42:58 +0800

    添加申请相机与读写储存权限

--- app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/SplashActivity.java
+++ app/src/main/java/com/example/baoxiaojianapp/baoxiaojianapp/activity/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.example.baoxiaojianapp.baoxiaojianapp.activity;
 
+import android.Manifest;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
@@ -8,22 +9,39 @@ import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.blankj.utilcode.util.PermissionUtils;
 import com.example.baoxiaojianapp.R;
 import com.example.baoxiaojianapp.baoxiaojianapp.Utils.MyApplication;
 import com.example.baoxiaojianapp.baoxiaojianapp.Utils.UserInfoCashUtils;
 
+import java.security.Permission;
+
 public class SplashActivity extends AppCompatActivity {
     private Context mcontext;
+    private static final int REQUEST_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mcontext=this;
         setContentView(R.layout.activity_splash);
-        process();
+        checkpermission();
+
+    }
+
+
+
+    private void checkpermission(){
+        if (PermissionUtils.isGranted(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+            process();
+        }else {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+        }
     }
     private void process(){
         if(UserInfoCashUtils.getUserLoginState()){
@@ -46,5 +64,13 @@ public class SplashActivity extends AppCompatActivity {
             }, 1000);
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==REQUEST_CODE){
+            checkpermission();
+        }
     }
 }
