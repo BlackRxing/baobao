9675c4bb8ba2a2656a13ae97391992d5144f9920
ricky <galxy1998922@163.com>
2019-12-12 15:49:58 +0800

    接入信鸽推送，小米系统级推送

---
+++ app/src/main/java/com/example/baoxiaojianapp/receiver/XinGeReceiver.java
@@ -0,0 +1,73 @@
+package com.example.baoxiaojianapp.receiver;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.blankj.utilcode.util.ToastUtils;
+import com.tencent.android.tpush.XGPushBaseReceiver;
+import com.tencent.android.tpush.XGPushClickedResult;
+import com.tencent.android.tpush.XGPushRegisterResult;
+import com.tencent.android.tpush.XGPushShowedResult;
+import com.tencent.android.tpush.XGPushTextMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class XinGeReceiver extends XGPushBaseReceiver {
+
+    public static final String LogTag = "TPushReceiver";
+    @Override
+    public void onRegisterResult(Context context, int i, XGPushRegisterResult xgPushRegisterResult) {
+
+    }
+
+    @Override
+    public void onUnregisterResult(Context context, int i) {
+
+    }
+
+    @Override
+    public void onSetTagResult(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onDeleteTagResult(Context context, int i, String s) {
+
+    }
+
+    @Override
+    public void onTextMessage(Context context, XGPushTextMessage xgPushTextMessage) {
+        // TODO Auto-generated method stub
+        String text = "收到消息:" + xgPushTextMessage.toString();
+        // 获取自定义key-value
+        String customContent = xgPushTextMessage.getCustomContent();
+        if (customContent != null && customContent.length() != 0) {
+            try {
+                JSONObject obj = new JSONObject(customContent);
+                // key1为前台配置的key
+                if (!obj.isNull("key")) {
+                    String value = obj.getString("key");
+                    Log.d(LogTag, "get custom value:" + value);
+                }
+                // ...
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d("LC", "++++++++++++++++透传消息");
+        // APP自主处理消息的过程...
+        Log.d(LogTag, text);
+        ToastUtils.showShort(text);
+    }
+
+    @Override
+    public void onNotifactionClickedResult(Context context, XGPushClickedResult xgPushClickedResult) {
+
+    }
+
+    @Override
+    public void onNotifactionShowedResult(Context context, XGPushShowedResult xgPushShowedResult) {
+
+    }
+}
